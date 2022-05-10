77aa30c59ad90c7bb656bac205ca3275ecd61a4c
ArturoVillegas3 <percentnevada3@gmail.com>
2022-05-10 14:02:21 -0500

    Tenth commit

--- src/main/java/com/connectPostgres/demoPostgres/DemoPostgresApplication.java
+++ src/main/java/com/connectPostgres/demoPostgres/DemoPostgresApplication.java
@@ -2,8 +2,10 @@ package com.connectPostgres.demoPostgres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class DemoPostgresApplication {
+public class DemoPostgresApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoPostgresApplication.class, args);
